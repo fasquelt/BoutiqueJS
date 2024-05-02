1acc2febee137c1f827c1828883cb8a1127b7ebb
Tom Fasquelle <tom.fasquelle@etu.univ-lyon1.fr>
2024-05-02 12:56:27 +0200

    Début du VCS

---
+++ data/catalogue.js
@@ -0,0 +1,50 @@
+var catalogue = [
+    {
+	"nom" : "chaise bleue",
+	"description" : "une chaise bleue avec 4 pieds",
+	"image" : "images/chaisebleue.jpg",
+	"prix" : "62"
+    },
+    {
+	"nom" : "chaise blanche",
+	"description" : "une chaise blanche matelassée avec 4 pieds noirs",
+	"image" : "images/chaiseblanche.jpg",
+	"prix" : "75"
+    },
+    {
+	"nom" : "chaise en bois",
+	"description" : "une chaise en bois  avec 4 pieds et un dossier rouge",
+	"image" : "images/chaisebois.jpg",
+	"prix" : "47"
+    },
+    {
+	"nom" : "chaise en cuir",
+	"description" : "une chaise en cuir vieilli avec un dossier confortable",
+	"image" : "images/chaisecuir.jpg",
+	"prix" : "125"
+    },
+    {
+	"nom" : "chaise design",
+	"description" : "une chaise design mais peut-être pas confortable",
+	"image" : "images/chaisedesign.jpg",
+	"prix" : "232"
+    },
+    {
+	"nom" : "chaise noire",
+	"description" : "une chaise noire avec un pied métallique",
+	"image" : "images/chaisenoire.jpg",
+	"prix" : "104"
+    },
+    {
+	"nom" : "chaise plastique",
+	"description" : "une chaise basique avec une coque plastique",
+	"image" : "images/chaiseplastique.jpg",
+	"prix" : "33"
+    },
+    {
+	"nom" : "chaise rouge",
+	"description" : "une chaise tout en plastique transparent rouge avec 4 pieds rouges aussi",
+	"image" : "images/chaiserouge.jpg",
+	"prix" : "52"
+    },
+]
